7b3f2252c841c0ab815023a4e74cb72c0b865b47
PauloJose26 <paulojoliveira.2616@gmail.com>
2021-12-09 17:52:23 -0300

    feat: Criaçao da funçao userUpdate e messagens de erros e de sucesso

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Routes from "./routes";
 import { Toaster } from "react-hot-toast";
 
+import { useUser } from "./providers/user";
 
 const App = () => {
+  const { } = useUser();
+
   return (
     <div className="App">
       <Routes />
-      <Toaster position="top-center" reverseOrder={false} />
+      <Toaster position="top-center" reverseOrder={false} 
+        toastOptions={ {
+          duration: 1500
+        } }/>
     </div>
   );
 };
--- src/providers/user/index.js
+++ src/providers/user/index.js
@@ -1,4 +1,6 @@
 import { createContext, useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import toast from "react-hot-toast";
 import jwt_decode from "jwt-decode";
 
 import api from "../../services/api";
@@ -6,41 +8,119 @@ import api from "../../services/api";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [ user, userState ] = useState({});
+    const [ user, userState ] = useState(null);
+    const [ users, usersState ] = useState([]);
+
+    const history = useHistory();
 
     const userRegister = (datas) => {
+        toast.loading("Espere...");
+
         api.post("/users/", datas)
-            .then(response => {})
-            .catch(error => {});
+            .then(() => {
+                toast.remove();
+                toast.success("Usuário Cadastrado");
+                history.push("/login");
+            })
+            .catch(error => {
+                toast.remove();
+                if(error.response.data.username){
+                    toast.error("Esse nome de usuário já existe");
+                }
+                else if(error.response.data.password){
+                    toast.error("Senha necessária");
+                }
+                else{
+                    toast.error("Erro, tente novamente mais tarde");
+                }
+            });
     }
 
     const userLogin = (datas) => {
+        toast.loading("Espere...");
+
         api.post("/sessions/", datas)
             .then(response => {
                 localStorage.setItem("@userToken", response.data.access);
+                toast.remove();
+                toast.success("Login realizado");
+                history.push("/");
+            })
+            .catch(error => {
+                toast.remove();
+                if(error.response.data.detail){
+                    toast.error("Usuário e/ou senha estão errados");
+                }
+                else if(error.response.data.password){
+                    toast.error("Senha necessária");
+                }
+                else{
+                    toast.error("Erro, tente novamente mais tarde");
+                }
+            });
+    }
+
+    const userUpdate = (datas) => {
+        const token = localStorage.getItem("@userToken");
+
+        if(token){
+            toast.loading("Espere...");
+            const decode_token = jwt_decode(token);
+            api.put(`/users/${ decode_token.user_id }`, datas, {
+                headers:{ Authorization: `Bearer ${token}` }
+            })
+                .then(response => {
+                    userState(response.data);
+                })
+                .catch(error => {
+                    if(error.response.data.message){
+                        toast.error("Você não tem permissão para editar esse usuário");
+                    }
+                    else if(error.response.data.username){
+                        toast.error("Esse usuário não existe");
+                    }
+                    else{
+                        toast.error("Erro, tente novamente mais tarde");
+                    }
+                    
+                    localStorage.removeItem("@userToken");
+                    userState(null);
+                    history.push("/login");
+                });
+        }
+    }
+
+    const getUser = () => {
+        const token = localStorage.getItem("@userToken");
+
+        if(token){
+            const decode_token = jwt_decode(token);
+            api.get(`/users/${ decode_token.user_id }/`)
+                .then(response => {
+                    userState(response.data);
+                })
+                .catch(() => {
+                    toast.error("Erro, tente novamente mais tarde");
+                    localStorage.removeItem("@userToken");
+                    userState(null);
+                });
+        }
+    }
+
+    const getUsers = (page) => {
+
+        api.get(`/users/${ (page)? `?page=${page}`: "" }`)
+            .then(response => {
+                usersState(response.data.results);
             })
             .catch(error => {
                 console.log(error);
             });
     }
 
-    const getUser = () => {
-        const token = localStorage.getItem("@userToken");
-        if(token){
-            const decode_token = jwt_decode(token);
-            api.get(`/users/${ decode_token.user_id }/`)
-                .then(response => {
-                    console.log(response.data)
-                    userState(response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        }
-    }
-
     return(
-        <UserContext.Provider value={ { user, userLogin, getUser, userRegister } }>
+        <UserContext.Provider value={ { user, users, userLogin, userRegister, 
+                                        userUpdate, getUser, getUsers } }>
             { children }
         </UserContext.Provider>
     );

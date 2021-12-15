35c2d78961c833957280a13ce472603b0aa3ad95
Matheus <mporto1994@gmail.com>
2021-12-15 20:53:01 -0300

    esqueleto das páginas adicionais completo

---
+++ src/pages/Group_Id/index.jsx
@@ -0,0 +1,39 @@
+import Button from "../../components/button";
+import Container from "../../components/container";
+import HomeImg from "../../assets/Newhome.png";
+import { useGroups } from "../../providers/groups";
+import CardGroup from "../../components/cardGroup";
+
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import { Div } from "./styles";
+
+const Group_Id = () => {
+  const history = useHistory();
+  const [ token, setToken ] = useState(null);
+  const { getGroups, groups } = useGroups();
+
+  const [ txtCategory, setCategory ] = useState("");
+  const [ txtSearch, setSearch ] = useState("");
+
+
+  useEffect(() => {
+    setToken(localStorage.getItem("@userToken"));
+    getGroups();
+  }, []);
+
+
+  return (
+    <>
+      {!token && 
+        <Container>
+          
+        </Container>
+      }
+      
+    </>
+  );
+};
+
+export default Group_Id;
---
+++ src/pages/Group_Id/styles.js
@@ -0,0 +1,79 @@
+import styled, { keyframes } from "styled-components";
+
+const appearFromLeft = keyframes`
+from{
+    opacity:0;
+    transform:translateX(50px)
+}
+to {
+    opacity: 1;
+    transform: translateX(0px);
+}
+`;
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 8px;
+  background-color: #ffb703;
+  width: 300px;
+  height: 350px;
+  animation: ${appearFromLeft} 2s;
+
+  div {
+    width: 100%;
+    display: flex;
+
+    flex-direction: column;
+    align-items: center;
+    margin: 25px 0 33px 0;
+
+    h1 {
+      margin: 10px 0 25px 0;
+    }
+  }
+`;
+
+export const Search = styled.section`
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 500px){
+    flex-direction: row;
+  }
+`;
+
+export const Section = styled.section`
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 30px 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const Input = styled.input`
+  width: 240px;
+  height: 40px;
+  padding-left: 10px;
+  color: black;
+  border-radius: 5px;
+  border: 1px solid gray;
+
+  &:focus {
+    outline-color: #219ebc;
+  }
+
+  &::placeholder {
+    color: gray;
+  }
+`;

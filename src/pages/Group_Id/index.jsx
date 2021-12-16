37335ff8cb4b6fb5c11babfb8f916a289434ae96
LuizPombo <luizhfpr@gmail.com>
2021-12-16 18:11:55 -0300

    feat: group_id

--- src/pages/Group_Id/index.jsx
+++ src/pages/Group_Id/index.jsx
@@ -3,6 +3,10 @@ import Container from "../../components/container";
 import HomeImg from "../../assets/Newhome.png";
 import { useGroups } from "../../providers/groups";
 import CardGroup from "../../components/cardGroup";
+import Users from "../../components/users";
+import GoalsCard from "../../components/goals";
+import ActivitiesCard from "../../components/activities";
+import { useParams } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
@@ -11,27 +15,27 @@ import { Div } from "./styles";
 
 const Group_Id = () => {
   const history = useHistory();
-  const [ token, setToken ] = useState(null);
-  const { getGroups, groups } = useGroups();
+  const [token, setToken] = useState(null);
+  const { getGroups, group, getGroupId } = useGroups();
+  const { id } = useParams();
 
-  const [ txtCategory, setCategory ] = useState("");
-  const [ txtSearch, setSearch ] = useState("");
-
+  const [txtCategory, setCategory] = useState("");
+  const [txtSearch, setSearch] = useState("");
 
   useEffect(() => {
     setToken(localStorage.getItem("@userToken"));
-    getGroups();
+    getGroupId(id);
   }, []);
-
 
   return (
     <>
-      {!token && 
+      {group && (
         <Container>
-          
+          <GoalsCard />
+          <ActivitiesCard />
+          <Users users={group.users_on_group} />
         </Container>
-      }
-      
+      )}
     </>
   );
 };
